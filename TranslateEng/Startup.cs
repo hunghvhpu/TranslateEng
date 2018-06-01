7ada2852666f1d8d16180b1b4a01f09fd965684f
hungh <hungh@DESKTOP-CMGSLRI>
2018-06-01 10:43:40 +0800

    Add project files.

---
+++ TranslateEng/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TranslateEng.Startup))]
+namespace TranslateEng
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
